d41db6e563d84e00c749fa620a1f5a74a0850119
zlalvani <zlalvani@gmail.com>
2015-11-15 22:49:33 -0500

    initial commit

---
+++ cosmotronic.py
@@ -0,0 +1,17 @@
+import sys
+import math as m
+import random
+
+def parser(input_file):
+	pass
+
+def main():
+	if len(sys.argv) != 2:
+		print "Usage: 'python cosmotronic.py [input-file]'"
+		exit()
+	
+	with open(sys.argv[1]) as input_file:
+		
+
+if __name__ == "__main__":
+	main()
